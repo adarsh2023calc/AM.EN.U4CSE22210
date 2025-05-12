a3a587e036e5b0c7c3d1eed13107aa0219140465
adarsh2023calc <adarshs2023@gmail.com>
2025-05-12 20:39:12 +0530

    Added more

--- Q1/main.py
+++ Q1/main.py
@@ -8,7 +8,20 @@ import requests
 
 
 
+from fastapi.middleware.cors import CORSMiddleware
+
+
+
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Numbers(BaseModel):
     value : str
